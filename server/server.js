a0f333b4d29c3d6f1695a5707edd1ff9adddaa69
Adinarayana Addanki <120593580+9949512061693@users.noreply.github.com>
2026-05-03 23:41:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,12 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      process.env.CLIENT_URL,
+      "http://localhost:5173"
+    ],
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());
