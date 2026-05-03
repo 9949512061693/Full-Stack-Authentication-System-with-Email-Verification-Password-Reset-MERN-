223a734d4b50396b34a10c77e14c85575d8d5794
Adinarayana Addanki <120593580+9949512061693@users.noreply.github.com>
2026-05-04 00:42:49 +0530

    Update EmailVerificationPage.jsx

--- client/src/pages/EmailVerificationPage.jsx
+++ client/src/pages/EmailVerificationPage.jsx
@@ -102,7 +102,7 @@ const EmailVerificationPage = () => {
             whileTap={{ scale: 0.95 }}
             type='submit'
             disabled={isLoading || code.some((digit) => !digit)}
-            className='w-full bg-gradient from-green-500 to-emerald-600 text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 disabled:opacity-50'
+            className='w-full bg-gradient from-green-500 to-emerald-600 border text-white font-bold py-3 px-4 rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 disabled:opacity-50'
           >
             {isLoading ? "Verifying..." : "Verify Email"}
           </motion.button>
